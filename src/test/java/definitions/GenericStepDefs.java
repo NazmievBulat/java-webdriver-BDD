1ad6300823bbc2d4019caaa268e1e09f1b0c69c2
bnazmiev <nazmievbulat@gmail.com>
2023-04-13 18:23:55 -0600

    new changes

--- src/test/java/definitions/GenericStepDefs.java
+++ src/test/java/definitions/GenericStepDefs.java
@@ -25,7 +25,7 @@ public class GenericStepDefs {
             case "calculator" -> getDriver().get("https://www.calculator.net");
             case "careers" -> new CareersHome().open();
             case "linkedin" -> getDriver().get("https://www.linkedin.com/checkpoint/rm/sign-in-another-account?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin");
-            case "job" -> getDriver().get("https://www.linkedin.com/jobs/search/?currentJobId=3448342127&distance=25&f_AL=true&f_WT=2&geoId=103644278&keywords=selenium");
+            case "job" -> getDriver().get("https://www.linkedin.com/jobs/search/?currentJobId=3529575194&f_AL=true&f_WT=2&geoId=103644278&keywords=selenium&location=United%20States&refresh=true");
             case "boeing" -> new BoeingHome().open();
 
             default -> throw new Error("Unsupported page " + page);
